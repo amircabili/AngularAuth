77ffb6c7c02e23d800edcc87ee4431a3adddb055
amir cabili <amircabili@hotmail.com>
2019-12-12 10:39:09 +0200

    update server.js

--- server/server.js
+++ server/server.js
@@ -6,9 +6,9 @@ const PORT = 3000
 const api = require('./routes/api')
 const app = express()
 
-app.use(cors())
+app.use(cors( ))
 
-app.use(bodyParser.json())
+app.use(bodyParser.json( ))
 
 app.use('/api',api)
 
@@ -17,7 +17,7 @@ app.get('/', function(req, res){
 })
  
 app.listen(PORT, function(){
-    console.log('Server running on localhost:' + PORT)
+    console.log('Server running on localhost:' + PORT) 
 })
 
  
